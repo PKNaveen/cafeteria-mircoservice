5a79a9768c70bcb1a9f62e4be05e0a3fe493d42a
PKNaveen <88645434+PKNaveen@users.noreply.github.com>
2025-09-30 21:00:41 +0530

    updated user-service tests, changed github-workflows for gateway and kafka-service

--- user-service/src/test/resources/test-user-data.sql
+++ user-service/src/test/resources/test-user-data.sql
@@ -1,8 +1,13 @@
 -- Write User sample data here
 
-TRUNCATE TABLE
-    users
-    RESTART IDENTITY CASCADE;
+
+CREATE TABLE users (
+                       user_id       UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                       user_name     TEXT NOT NULL,
+                       user_email    TEXT NOT NULL UNIQUE,
+                       image_url     TEXT NOT NULL,
+                       created_date  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 INSERT INTO users (user_id, user_name, user_email, image_url, created_date)
 VALUES ('11111111-1111-1111-1111-111111111111', 'Alice Johnson', 'alice.johnson@example.com', 'https://example.com/images/alice.jpg', NOW());
